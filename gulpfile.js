b3cb55ea00cb9cae9863e16b932646e79732720c
Иван <snake07.07@mail.ru>
2017-09-13 23:25:48 +0400

    add beauty in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,40 @@
-var gulp = require('gulp'),
+var 	gulp = require('gulp'),
 	browserSync = require('browser-sync'),
 	babel = require('gulp-babel'),
 	clean = require('gulp-clean');
 
-
-
-
 gulp.task('browserSync', function() {
-browserSync({
-server: {
-baseDir: 'dest'},
-})
+	browserSync({
+		server: {
+		baseDir: 'dest'},
+	})
 });
 
 gulp.task('css', function () {
-    return gulp.src('./src/css/*.css')
+    	return gulp.src('./src/css/*.css')
         .pipe(gulp.dest('./dest/css/'));
 });
 
 gulp.task('babell', () =>
 	gulp.src('src/js/script.js')
-		.pipe(babel({
-			presets: ['env']
-		}))
-		.pipe(gulp.dest('dest/js'))
+	.pipe(babel({
+		presets: ['env']
+	}))
+	.pipe(gulp.dest('dest/js'))
 );
 
 gulp.task('clean', function () {
 	return gulp.src('dest/', {read: false})
-		.pipe(clean());
+	.pipe(clean());
 });
 
 gulp.task('copy_html', function() {
-return gulp.src('src/**/*.html')
-.pipe(gulp.dest('dest/'))
+	return gulp.src('src/**/*.html')
+	.pipe(gulp.dest('dest/'))
 })
 
 gulp.task('watch', ['browserSync', 'clean', 'copy_html', 'css', 'babell'], function (){
-gulp.watch('src/css/**/*.css', ['css']);
-gulp.watch('src/*.html', ['copy_html', browserSync.reload]);
-gulp.watch('src/js/**/*.js', ['babell', browserSync.reload]);
+	gulp.watch('src/css/**/*.css', ['css']);
+	gulp.watch('src/*.html', ['copy_html', browserSync.reload]);
+	gulp.watch('src/js/**/*.js', ['babell', browserSync.reload]);
 });
